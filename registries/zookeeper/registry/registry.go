de9f55ad3d75038938f627e8d03270d5c84d017c
leiliang <1354370192@qq.com>
2024-03-27 11:13:47 +0800

    fix(resolve): added cm (#86)

--- registries/zookeeper/registry/registry.go
+++ registries/zookeeper/registry/registry.go
@@ -90,7 +90,7 @@ func (z *zookeeperRegistry) Register(info *registry.Info) error {
 	ctx, cancel := context.WithCancel(context.Background())
 	z.canceler.add(finalPath, cancel)
 
-	if !z.opt.KeepAlive {
+	if z.opt.KeepAlive {
 		go z.keepalive(ctx, finalPath, nil)
 	}
 
